35164697439fde358fbd1ba040ffddd264fbfbfe
yfigueira <figueirapy@gmail.com>
2025-03-30 15:47:36 +0200

    project base

---
+++ demo/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(add(2, 3));
+    }
+
+    public static int add(int first, int second) {
+        return first + second;
+    }
+}
