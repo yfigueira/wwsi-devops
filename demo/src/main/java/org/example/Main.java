657967f075d970888b7d03d5b2af238a972d453d
yfigueira <figueirapy@gmail.com>
2025-03-30 15:56:47 +0200

    added subtraction

--- demo/src/main/java/org/example/Main.java
+++ demo/src/main/java/org/example/Main.java
@@ -3,9 +3,14 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println(add(2, 3));
+        System.out.println(subtract(3, 2));
     }
 
     public static int add(int first, int second) {
         return first + second;
+    }
+
+    public static int subtract(int first, int second) {
+        return first - second;
     }
 }
