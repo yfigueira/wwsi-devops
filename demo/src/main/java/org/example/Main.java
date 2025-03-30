bcffc2c3e89e6a95c26e1318c43c888bfa261603
yfigueira <figueirapy@gmail.com>
2025-03-30 16:06:41 +0200

    added division

--- demo/src/main/java/org/example/Main.java
+++ demo/src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(add(2, 3));
         System.out.println(subtract(3, 2));
+        System.out.println(divide(4, 2));
     }
 
     public static int add(int first, int second) {
@@ -12,5 +13,9 @@ public class Main {
 
     public static int subtract(int first, int second) {
         return first - second;
+    }
+
+    public static double divide(int first, int second) {
+        return (double) first / (double) second;
     }
 }
